8cb83bc6c71f893c2f8adcf4d31caf0503eb35b7
sanjeet <sanjeet.love6@gmail.com>
2017-10-02 16:35:40 +0530

    responsive images

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ postCss = require('gulp-postcss'),
 autopreFixer = require('autoprefixer'),
 simpleVars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
  return gulp.src('./app/assets/css/style.css')
-     .pipe(postCss([cssImport, simpleVars, nested, autopreFixer]))
+     .pipe(postCss([cssImport, mixins, simpleVars, nested, autopreFixer]))
      .on('error', function(errorInfo){
      console.log(errorInfo.toString());
      this.emit('end');
